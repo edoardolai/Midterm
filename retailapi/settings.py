032b0eeb4aa4171666b1597d4c59b8a1c0507cb7
edoardolai <edoardolai17@gmail.com>
2026-07-05 21:18:37 +0200

    set debugging true locally only

--- retailapi/settings.py
+++ retailapi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
